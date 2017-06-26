3d494ecd8e22eff06a78e703a14c9a8180784695
prince <prince@payhind.com>
2017-06-26 06:55:40 +0530

    Changes make after 2nd Review

--- code/media.py
+++ code/media.py
@@ -1,19 +1,33 @@
-import webbrowser
+# coding=utf-8
 
-"""movie class having 4 argument 1. movie_title:- passing movie name
-2. movie_storyline:- passing only story line in one line
-3. poster_image:- passing url of poster of movie
-4. trailer_youtube:- passing the youtube url of movie trailer"""
+import webbrowser
 
 
 class Movie():
+    """
+    This class provides a way to store movie related information
+    """
 
-    def __init__(self, movie_title, movie_storyline, poster_image, trailer_youtube):
+    def __init__(self, movie_title, movie_storyline,
+                 poster_image, trailer_youtube):
+        """
+        initialize instance of class Movie
+
+        :param movie_title: title
+        :param movie_storyline: storyline
+        :param poster_image: poster_image_url
+        :param trailer_youtube: trailer_youtube_url
+        """
         self.title = movie_title
         self.storyline = movie_storyline
         self.poster_image_url = poster_image
         self.trailer_youtube_url = trailer_youtube
 
-# this function open trailer of movie in browser
+    # this function open trailer of movie in browser
     def show_trailer(self):
+        """
+        initializing instance for opening the youtube video
+
+        :return: webbrowser to play thriller
+        """
         webbrowser.open(self.trailer_youtube_url)
